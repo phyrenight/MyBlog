9da574482b2f78da1639bc25ad4eb393c47ed77c
Marc Preston <phyrenight@gmail.com>
2018-04-29 05:42:02 -0400

    add generic routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,11 +1,46 @@
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from app import app
+from app import db
+from app.models import User, Post, Comment
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-from app import routes, models
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def sign_in():
+    return render_template('signin.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/posts')
+def get_all_post():
+    return render_template('get_all_post.html')
+
+
+@app.route('/posts/<post_id>', methods=['GET', 'POST'])
+def get_a_post(post_id):
+    return render_template('a_post.html')
+
+
+@app.route('/posts/<user_id>/<post_id>/delete', methods=['GET', 'POST'])
+def delete_a_post(user_id, post_id):
+    return render_template('delete_post.html')
+
+
+@app.route('/posts/<user_id>/createpost', methods=['GET', 'POST'])
+def create_a_post(user_id):
+    return render_template('create_post.html')
+
+
+@app.route('/posts/<user_id>/<post_id>/editpost', methods=['GET', 'POST'])
+def edit_a_post(user_id, post_id):
+    return render_template('edit_post.html')
+
+
